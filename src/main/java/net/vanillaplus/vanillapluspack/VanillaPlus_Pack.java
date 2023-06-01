b08438fe47e00e42634bf580f57c19e6b70cb559
GoldenEdit <sammyhmatthews@gmail.com>
2023-06-01 11:33:16 +0200

    Updates

--- src/main/java/net/vanillaplus/vanillapluspack/VanillaPlus_Pack.java
+++ src/main/java/net/vanillaplus/vanillapluspack/VanillaPlus_Pack.java
@@ -3,7 +3,6 @@ package net.vanillaplus.vanillapluspack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VanillaPlus_Pack extends JavaPlugin {
-
     @Override
     public void onEnable() {getCommand("pack").setExecutor(new Command());}
 }
